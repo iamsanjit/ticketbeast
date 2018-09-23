806090d6fe5444e908d5522a7ce5358a1d31af27
Sanjit <iamsanjit@hotmail.com>
2018-09-23 13:16:41 -0400

    List chracterstics of order confimation number generator

---
+++ tests/Unit/RandomOrderConfimationNumberGeneratorTest.php
@@ -0,0 +1,11 @@
+<?php 
+
+use Illuminate\Foundation\Testing\TestCase;
+
+class RandomOrderConfimationNumberGeneratorTest extends TestCase
+{
+    // Can contain uppercase letters and numbers
+    // Cannot contain ambiguis chracters
+    // Must be 24 characters long
+    // All order confirmation number must be unique
+}
