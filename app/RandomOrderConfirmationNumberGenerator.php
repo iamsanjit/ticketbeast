1b557cbbcf24fac79fde91cc78b9be4610d125c5
Sanjit <iamsanjit@hotmail.com>
2018-09-29 10:48:39 -0400

    Testing confirmation format and ensuring uniqueness

---
+++ app/RandomOrderConfirmationNumberGenerator.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+class RandomOrderConfirmationNumberGenerator implements OrderConfirmationNumberGenerator
+{
+    public function generate()
+    {
+        $pool = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789';
+        $length = 24;
+        return substr(str_shuffle(str_repeat($pool, $length)), 0, $length);
+    }
+}
--- tests/Unit/RandomOrderConfimationNumberGeneratorTest.php
+++ tests/Unit/RandomOrderConfimationNumberGeneratorTest.php
@@ -1,11 +1,56 @@
 <?php 
 
-use Illuminate\Foundation\Testing\TestCase;
+use Tests\TestCase;
+use App\RandomOrderConfirmationNumberGenerator;
 
 class RandomOrderConfimationNumberGeneratorTest extends TestCase
 {
-    // Can contain uppercase letters and numbers
     // Cannot contain ambiguis chracters
-    // Must be 24 characters long
     // All order confirmation number must be unique
+
+    /** @test */
+    public function must_be_24_chracters_long()
+    {
+        $generator = new RandomOrderConfirmationNumberGenerator;
+
+        $confirmationNumber = $generator->generate();
+        
+        $this->assertEquals(24, strlen($confirmationNumber));
+    }
+
+    /** @test */
+    public function must_contain_uppercase_letters_and_numbers()
+    {
+        $generator = new RandomOrderConfirmationNumberGenerator;
+
+        $confirmationNumber = $generator->generate();
+
+        $this->assertRegExp('/^[A-Z0-9]+$/', $confirmationNumber);
+    }
+
+    /** @test */
+    public function cannot_contain_ambiguis_characters()
+    {
+        $generator = new RandomOrderConfirmationNumberGenerator;
+
+        $confirmationNumber = $generator->generate();
+
+        $this->assertFalse(strpos($confirmationNumber, '1'));
+        $this->assertFalse(strpos($confirmationNumber, 'I'));
+        $this->assertFalse(strpos($confirmationNumber, 'O'));
+        $this->assertFalse(strpos($confirmationNumber, '0'));
+    }
+
+    /** @test */
+    public function must_generate_unique_confirmaiton_numbers()
+    {
+        $generator = new RandomOrderConfirmationNumberGenerator;
+        
+        $confirmationNumbers = array_map(function ($i) use ($generator) {
+            return $generator->generate();
+        }, range(1, 100));
+
+        dd($confirmationNumbers);
+        $this->assertCount(100, array_unique($confirmationNumbers));
+    }
 }
